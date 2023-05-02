74ea35ae1afd1cb3b367955a5499084d6636978a
Bonkesam <bonkesam9@gmail.com>
2023-05-02 20:05:40 +0300

    Adds the Navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
-import { getSession, signOut } from 'next-auth/react';
+import { getSession } from 'next-auth/react';
 import { NextPageContext } from 'next';
-import useCurrentUser from '@/hooks/useCurrentUser';
+import Navbar from '@/components/Navbar';
+
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -20,15 +21,11 @@ export async function getServerSideProps(context: NextPageContext) {
 
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
-
-
+ 
   return (
     <>
-    <h1 className="text-white text-2xl">Netflix Clone</h1>
-    <p className='text-white text-2xl'>Logged in as : {user?.email}</p>
-    <button className='h-10 w-full bg-white' onClick={() => signOut()}>Logout</button>
-      
+    <Navbar />
+
     </>
   )
 }
